98833a0f2b8bfb61d42f9b0342f1d502823bc948
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2018-08-15 13:48:40 +0200

    add member group controller to config

--- eco-feature/eco-feature-members/src/main/kotlin/community/flock/eco/feature/members/MemberConfiguration.kt
+++ eco-feature/eco-feature-members/src/main/kotlin/community/flock/eco/feature/members/MemberConfiguration.kt
@@ -1,6 +1,7 @@
 package community.flock.eco.feature.members
 
 import community.flock.eco.feature.members.controllers.MemberController
+import community.flock.eco.feature.members.controllers.MemberGroupController
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
@@ -9,5 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @Configuration
 @EnableJpaRepositories
 @EntityScan
-@Import(MemberController::class)
+@Import(
+        MemberController::class,
+        MemberGroupController::class)
 class MemberConfiguration
