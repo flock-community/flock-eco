0fc5bb58a1358a75505e0acabb1dc18f97e59b5c
Willem Kersten <willem.kersten@ing.com>
2018-08-17 14:26:07 +0200

    Add fields to Memberform

--- eco-feature/eco-feature-member/src/main/react/member/MemberForm.js
+++ eco-feature/eco-feature-member/src/main/react/member/MemberForm.js
@@ -10,19 +10,25 @@ class MemberForm extends React.Component {
     this.init(props.item)
   }
 
-  init(obj) {
+  init(memberData) {
     const member = {
-      firstName: "",
-      infix: "",
-      surName: "",
-      email: "",
+        firstName: "",
+        infix: "",
+        surName: "",
+        gender: "",
+        birthDate: "",
+        email: "",
+        street: "",
+        houseNumber: "",
+        houseNumberExtension: "",
+        postalCode: "",
+        city: "",
     };
 
-    this.state = Object.assign(member, obj);
+    this.state = {...member, ...memberData};
   }
 
   render() {
-
     const {classes, item} = this.props;
 
     return (
@@ -43,14 +49,44 @@ class MemberForm extends React.Component {
           onChange={this.handleChange('infix')}/>
 
         <TextField
-          label="Sur name"
+          label="Surname"
           value={this.state.surName}
           onChange={this.handleChange('surName')}/>
+
+        <TextField
+          label="Gender"
+          value={this.state.gender}
+          onChange={this.handleChange('gender')}/>
+
+        <TextField
+          label="Birth date"
+          value={this.state.birthDate}
+          onChange={this.handleChange('birthDate')}/>
 
         <TextField
           label="Email"
           value={this.state.email}
           onChange={this.handleChange('email')}/>
+
+        <TextField
+          label="House number"
+          value={this.state.houseNumber}
+          onChange={this.handleChange('houseNumber')}/>
+
+        <TextField
+          label="House number extension"
+          value={this.state.houseNumberExtension}
+          onChange={this.handleChange('houseNumberExtension')}/>
+
+        <TextField
+          label="Postal code"
+          value={this.state.postalCode}
+          onChange={this.handleChange('postalCode')}/>
+
+        <TextField
+          label="City"
+          value={this.state.city}
+          onChange={this.handleChange('city')}/>
 
       </Grid>
     )
@@ -61,7 +97,6 @@ class MemberForm extends React.Component {
       this.setState(
         {[name]: event.target.value},
         () => {
-          console.log(this.state)
           this.props.onChange(this.state)
         })
 
