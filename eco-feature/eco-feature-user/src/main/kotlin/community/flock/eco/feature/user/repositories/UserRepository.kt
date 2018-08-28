88022087586619e51f1cd6d325915eaf89b4729e
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2018-08-28 11:29:51 +0200

    make find by user optional

--- eco-feature/eco-feature-user/src/main/kotlin/community/flock/eco/feature/user/repositories/UserRepository.kt
+++ eco-feature/eco-feature-user/src/main/kotlin/community/flock/eco/feature/user/repositories/UserRepository.kt
@@ -3,12 +3,13 @@ package community.flock.eco.feature.user.repositories
 import community.flock.eco.feature.user.model.User
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Service
+import java.util.*
 
 
 @Service
 interface UserRepository : PagingAndSortingRepository<User, Long> {
 
-    fun findByReference(name: String): User?
+    fun findByReference(name: String): Optional<User>
 
 }
 
