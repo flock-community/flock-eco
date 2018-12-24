9ce5856ccf9ea01af340c1239f02b524909e3673
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2018-12-24 14:27:03 +0100

    rm ServletInitializer

--- eco-feature/eco-feature-member/src/main/kotlin/community/flock/eco/feature/member/MemberApplication.kt
+++ eco-feature/eco-feature-member/src/main/kotlin/community/flock/eco/feature/member/MemberApplication.kt
@@ -1,17 +1,16 @@
 package community.flock.eco.feature.member
 
 import community.flock.eco.feature.member.data.MemberLoadData
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Import
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import javax.annotation.PostConstruct
 
 
 @SpringBootApplication
 @Import(MemberConfiguration::class, MemberLoadData::class)
-class MemberApplication: SpringBootServletInitializer() {
+class MemberApplication {
 
     @Autowired
     lateinit var memberLoadData: MemberLoadData
