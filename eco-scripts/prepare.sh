6c5764e57747a2962ee39c73e171ed38a3245fbd
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2019-04-05 21:11:23 +0200

    ci: script

--- eco-scripts/prepare.sh
+++ eco-scripts/prepare.sh
@@ -2,6 +2,14 @@ git_reset () {
   git reset --hard
 }
 
+tag_name () {
+  echo "v$1"
+}
+
+commit_message () {
+  echo "release version: $1"
+}
+
 check_changes () {
   if ! git diff-index --quiet HEAD --; then
     echo "Uncommitted changes found";
@@ -21,7 +29,11 @@ set_version() {
 check_changes
 version=$(set_version $@)
 
-echo "Set version: $version"
+echo "Set version: ${version}"
+echo "Tag name: $(tag_name ${version})"
 
 npm run lerna -- version --yes --no-git-tag-version ${version}
 mvn versions:set -DnewVersion=${version} -DgenerateBackupPoms=false
+
+git add .
+git commit -m $(commit_message ${version})
