102cf9b9aea74554b3984b0a1b67405685ccb967
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2019-04-05 19:30:46 +0200

    update script

--- eco-scripts/release-prepare.sh
+++ eco-scripts/release-prepare.sh
@@ -1,10 +1,16 @@
+git_reset () {
+  git reset --hard
+}
+
 if ! git diff-index --quiet HEAD --; then
-    echo "Uncommitted changes found"; exit 0;
+    echo "Uncommitted changes found";
+    exit 0;
 fi
 
 if ! [ "$1" != "" ]; then
-    echo "No version set"; exit 0;
-fi
+    echo "No version set";
+    exit 0;
+f
 
 echo "Version: $1"
 npx lerna version $1
