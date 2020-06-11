bb64c379eceff383944cb2b59f406b8729772795
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2020-06-11 09:25:27 +0200

    fix: change scope to @flock-community

--- eco-feature/eco-feature-user/src/main/react/user_group/UserGroupDialog.js
+++ eco-feature/eco-feature-user/src/main/react/user_group/UserGroupDialog.js
@@ -13,7 +13,7 @@ import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import UserGroupClient from './UserGroupClient'
-import {ConfirmDialog} from '@flock-eco/core/src/main/react/components/ConfirmDialog'
+import {ConfirmDialog} from '@flock-community/flock-eco-core/src/main/react/components/ConfirmDialog'
 import {Snackbar} from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
