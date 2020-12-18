50417b2e1820326bd87fbf7a1326577ea2dbcc08
Willem Veelenturf <willem.veelenturf@gmail.com>
2020-12-18 09:35:23 +0100

    Project setup

---
+++ eco-application/eco-application-example/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: path.join(process.cwd(), 'src/main/react/index.html'),
+    filename: './index.html',
+})
+
+module.exports = {
+    entry: path.join(process.cwd(), 'src/main/react'),
+
+    output: {
+        publicPath: '/',
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'target/classes/static'),
+    },
+
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules[\\\/](?!(@flock-eco)[\\\/]).*/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-transform-runtime',
+                        ],
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript',
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+
+    plugins: [htmlPlugin],
+
+    devServer: {
+        port: 3000,
+        host: 'localhost',
+        proxy: {
+            '/api/**': 'http://localhost:8080',
+            '/graphql': 'http://localhost:8080',
+            '/login': 'http://localhost:8080',
+        },
+    },
+}
