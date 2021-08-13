106d0d061d4aec64e2210e920fbe7b5a491e74c8
Willem Veelenturf <willem.veelenturf@gmail.com>
2021-08-13 13:33:36 +0200

    Fromat

--- eco-application/eco-application-multi_tenant/src/test/kotlin/CrossLoginTenantTest.kt
+++ eco-application/eco-application-multi_tenant/src/test/kotlin/CrossLoginTenantTest.kt
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockHttpSession
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
