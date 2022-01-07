f05bd4cec452851a83aad726053904aa00a15d94
Willem Veelenturf <willem.veelenturf@gmail.com>
2022-01-07 16:00:48 +0100

    clean

--- eco-core/src/main/react/clients/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.ResourceClient = exports.PageableClient = void 0;
-var PageableClient_1 = require("./PageableClient");
-exports.PageableClient = PageableClient_1.PageableClient;
-var ResourceClient_1 = require("./ResourceClient");
-exports.ResourceClient = ResourceClient_1.ResourceClient;
